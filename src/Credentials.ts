2fd2751a752b8512eb6578f1a96f16a162103c3e
Per Fahén <perfahlen@gmail.com>
2017-08-08 10:01:12 +0200

    Update Credentials.ts
    alter credentials

--- src/Credentials.ts
+++ src/Credentials.ts
@@ -1,10 +1,10 @@
 
 export class Credentials{
     static get BMCredentials(): string{
-        return "AsXOzwxphj5MnBu0JvpoF7joDb6BdaAa8NHUjUbHj-S9n-_1DzS3vTHfSVmVyXnn";
+        return "YOUR BING MAPS KEY";
     }
 
     static get NanjingKey(): string{
-        return "bc020229cdde4f47a513d47993018571";
+        return "YOUR KEY FROM COGNITIVE SERVICES";
     }
 }
